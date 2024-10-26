8dbccd541503686e2fd6b406d12add096487cab0
André Ghisleni Raimann <andre@andreg.com.br>
2024-10-26 19:03:50 -0300

    Changes to be committed: 	modified:   apps/web/src/components/summary/total-tickets-without-delivered.tsx

--- apps/web/src/components/summary/total-tickets-without-delivered.tsx
+++ apps/web/src/components/summary/total-tickets-without-delivered.tsx
@@ -21,7 +21,7 @@ export async function TotalTicketWithoutDelivered() {
   const totalToDeliver =
     totalTickets -
     totalDeliveredTickets -
-    diferenceBetweenTotalWithCriticaAndDeliveredAndTotalWithCritica
+    Math.abs(diferenceBetweenTotalWithCriticaAndDeliveredAndTotalWithCritica)
 
   return (
     <Card>
